e359c3a2afb93ecc49e1a095534cb29ef3133ee2
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2024-01-26 21:49:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,65 @@
 // script.js
 
-// Function to apply user preferences and store them in cookies
-function applyPreferences(fontSize, fontColor) {
-    document.body.style.fontSize = `${fontSize}px`;
-    document.body.style.color = fontColor;
-
-    // Store preferences in cookies
-    document.cookie = `fontsize=${fontSize}; expires=Sun, 31 Dec 2023 12:00:00 UTC; path=/`;
-    document.cookie = `fontcolor=${fontColor}; expires=Sun, 31 Dec 2023 12:00:00 UTC; path=/`;
-}
-
-// Function to retrieve user preferences from cookies
-function getPreferences() {
-    const cookies = Object.fromEntries(document.cookie.split('; ').map(cookie => cookie.split('=')));
-    return cookies;
-}
-
-// Apply stored preferences on page load
-window.onload = function () {
-    const preferences = getPreferences();
-
-    if (preferences.fontsize) {
-        const { fontsize, fontcolor } = preferences;
-        document.getElementById('fontsize').value = fontsize;
-        applyPreferences(fontsize, fontcolor);
+document.addEventListener("DOMContentLoaded", function () {
+  // Function to set cookies
+  function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+      expires = "; expires=" + date.toUTCString();
     }
-};
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+  }
 
-// Event listener for form submission
-document.getElementById('preferences-form').addEventListener('submit', function (event) {
+  // Function to get cookies
+  function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == " ") c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
+
+  // Function to handle form submission
+  function handleFormSubmit(event) {
     event.preventDefault();
 
-    const fontSize = document.getElementById('fontsize').value;
-    const fontColor = document.getElementById('fontcolor').value;
+    // Get values from the form
+    var fontSize = document.getElementById("fontsize").value;
+    var fontColor = document.getElementById("fontcolor").value;
 
-    applyPreferences(fontSize, fontColor);
+    // Set CSS variables and update styles
+    document.documentElement.style.setProperty("--fontsize", fontSize + "px");
+    document.documentElement.style.setProperty("--fontcolor", fontColor);
+
+    // Save preferences in cookies
+    setCookie("fontsize", fontSize, 365);
+    setCookie("fontcolor", fontColor, 365);
+  }
+
+  // Function to apply saved preferences on page load
+  function applySavedPreferences() {
+    var savedFontSize = getCookie("fontsize");
+    var savedFontColor = getCookie("fontcolor");
+
+    if (savedFontSize) {
+      document.getElementById("fontsize").value = savedFontSize;
+      document.documentElement.style.setProperty("--fontsize", savedFontSize + "px");
+    }
+
+    if (savedFontColor) {
+      document.getElementById("fontcolor").value = savedFontColor;
+      document.documentElement.style.setProperty("--fontcolor", savedFontColor);
+    }
+  }
+
+  // Add event listener to the form for submission
+  document.querySelector("form").addEventListener("submit", handleFormSubmit);
+
+  // Apply saved preferences on page load
+  applySavedPreferences();
 });
